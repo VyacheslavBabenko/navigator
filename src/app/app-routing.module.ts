0c5a6cb6d5f12c1f268c2f3fcf987d8e34a2921d
Vyacheslav <mr.aivengo@gmail.com>
2020-01-07 04:22:27 +0500

    добавление файлов в проект

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ItemComponent } from './item/item.component';
 
 
-const routes: Routes = [];
+const appRoutes: Routes = [
+  { path: 'navigator', component: ItemComponent },
+]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
